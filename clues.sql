0c59dbd098298f26870710bbb2676b86ae2a3440
haiaza <ajames1342@gmail.com>
2024-11-03 09:04:57 -0500

    Cat stepped on the KB. 4, 5 and 6 are complete

--- clues.sql
+++ clues.sql
@@ -54,6 +54,8 @@ SELECT capital FROM countries WHERE code = 'BRA';
 -- 211
 SELECT name FROM cities WHERE id = '211';
 --'Brasília' 
+
+
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
 
